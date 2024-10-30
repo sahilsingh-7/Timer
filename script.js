1927b78c6a48a5ecd15f973a36bf185a446ba09e
sahilsingh-7 <164621582+sahilsingh-7@users.noreply.github.com>
2024-10-30 11:30:30 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-let intervalId = null;
+const hr = document.getElementById('h1');
+const min = document.getElementById('h2');
+const sec = document.getElementById('h3');
+let hours = parseInt(prompt("Enter Hours :"),10)
+let minutes = parseInt(prompt("Enter Minutes :"),10);
+let seconds = parseInt(prompt("Enter Seconds :"),10);
 
-document.getElementById('startBtn').addEventListener('click', function() {
-    if (!intervalId) {
-        intervalId = setInterval(playAlarm, 300000); // 300000 milliseconds = 5 minutes
-        playAlarm(); // Play immediately when the alarm starts
-    }
-});
-
-document.getElementById('stopBtn').addEventListener('click', function() {
-    if (intervalId) {
-        clearInterval(intervalId);
-        intervalId = null;
-    }
-});
-
-function playAlarm() {
-    let alarmSound = document.getElementById('alarmSound');
-    alarmSound.play();
+function updateTime(){
+    seconds--;
+                    //keep in mind that user can provide wring input as well so tackel that situation 
+  if(seconds<0){
+    seconds=59;
+    minutes--;
+  }
+  if(minutes<0){
+    minutes = 59;
+    hours--;
+  }
+  if(hours<0){
+    clearInterval(intervalId);
+    hours = 0;
+    minutes=0;
+    seconds = 0;
+  }
+ 
+  hr.textContent = hours;
+  min.textContent = minutes;
+  sec.textContent = seconds;
 }
+let intervalId = setInterval(updateTime,1000);
